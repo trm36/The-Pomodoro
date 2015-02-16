ec10df4590e0a7be98a3a211bd29d3f760b24a40
Taylor Mott <trm36@humboldt.edu>
2015-02-16 12:08:41 -0700

    STEP 3: Adds POTimer Class
    Signed-off-by: Taylor Mott <trm36@humboldt.edu>

--- The Pomodoro/TimerViewController.m
+++ The Pomodoro/TimerViewController.m
@@ -8,6 +8,7 @@
 //
 
 #import "TimerViewController.h"
+#import "POTimer.h"
 
 @interface TimerViewController ()
 
@@ -19,15 +20,46 @@
 
 @implementation TimerViewController
 
+-(instancetype)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    
+    if (self)
+    {
+        [self registerForNotifications];
+    }
+    
+    return self;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+}
+
+- (void)dealloc
+{
+    [self unregisterForNotifications];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+#pragma mark - Notification Registration
+
+-(void)registerForNotifications
+{
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(updateTimerLabel) name:secondTickNotification object:nil];
+}
+
+-(void)unregisterForNotifications
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+#pragma mark - Timer Methods
 
 - (IBAction)toggleTimer
 {
@@ -39,8 +71,43 @@
     {
         self.timerIsOn = YES;
         self.timerButton.enabled = NO;
+        [[POTimer sharedInstance] startTimer];
     }
     
+}
+
+- (void)updateTimerLabel
+{
+    NSInteger minutes = [POTimer sharedInstance].minutes;
+    NSInteger seconds = [POTimer sharedInstance].seconds;
+    
+    self.timerLabel.text = [self timerStringWithMinutes:minutes andSeconds:seconds];
+}
+
+- (NSString *)timerStringWithMinutes:(NSInteger)minutes andSeconds:(NSInteger)seconds
+{
+    NSString *timerString;
+    
+    if (minutes >= 10)
+    {
+        timerString = [NSString stringWithFormat:@"%li:", (long)minutes];
+    }
+    else
+    {
+        timerString = [NSString stringWithFormat:@"0%li:", (long)minutes];
+    }
+    
+    if (seconds >= 10)
+    {
+        timerString = [timerString stringByAppendingString:[NSString stringWithFormat:@"%li", (long)seconds]];
+    }
+    else
+    {
+        timerString = [timerString stringByAppendingString:[NSString stringWithFormat:@"0%li", (long)seconds]];
+    }
+    
+    return timerString;
+
 }
 
 /*
