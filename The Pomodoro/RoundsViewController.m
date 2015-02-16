aade84cd5e2551813f38f0f8ec21d46e97cc00ae
Taylor Mott <trm36@humboldt.edu>
2015-02-16 12:46:19 -0700

    STEP 4: RoundsViewController tableview added
    Signed-off-by: Taylor Mott <trm36@humboldt.edu>

--- The Pomodoro/RoundsViewController.m
+++ The Pomodoro/RoundsViewController.m
@@ -7,8 +7,13 @@
 //
 
 #import "RoundsViewController.h"
+#import "POTimer.h"
 
-@interface RoundsViewController ()
+static NSString *reuseID = @"reuseID";
+
+@interface RoundsViewController () <UITableViewDataSource, UITableViewDelegate>
+
+@property (strong, nonatomic) UITableView *tableView;
 
 @end
 
@@ -17,12 +22,57 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    self.tableView = [[UITableView alloc] initWithFrame:self.view.bounds];
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
+    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:reuseID];
+    [self.view addSubview:self.tableView];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+#pragma mark - Table View Data Source Methods
+
+-(NSArray *)roundTimes
+{
+    return @[@25, @5, @25, @5, @25, @5, @25, @15];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:reuseID];
+    
+    cell.textLabel.text = [NSString stringWithFormat:@"%@ minutes", [self roundTimes][indexPath.row]];
+    
+    return cell;
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [self roundTimes].count;
+}
+
+#pragma mark - Table View Delegate Methods
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [self roundSelectedAtRow:indexPath.row];
+}
+
+#pragma mark - Notifications Methods
+
+-(void)roundSelectedAtRow:(NSInteger)index
+{
+    [POTimer sharedInstance].minutes = ((NSInteger)[self roundTimes][index]);
+    [POTimer sharedInstance].seconds = 0;
+    
+    [[NSNotificationCenter defaultCenter] postNotificationName:currentRoundNotification object:nil];
+}
+
 
 /*
 #pragma mark - Navigation
